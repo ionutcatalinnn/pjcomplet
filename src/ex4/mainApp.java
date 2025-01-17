de59b278ba02bf0965a993d8b151116992e222b7
ionutcatalinnn <126414603+ionutcatalinnn@users.noreply.github.com>
2025-01-17 15:54:13 +0200

    Add files via upload

---
+++ src/ex4/mainApp.java
@@ -0,0 +1,43 @@
+package ex4;
+
+import java.util.Random;
+
+public class mainApp {
+    public static void Cmmdc(int n, int m ) {
+        while(n != m)
+        {
+            if( n > m)
+            {
+                n = n-m;
+            }
+            else if (m > n)
+            {
+                m = m - n;
+            }
+            if (m == n)
+            {
+                System.out.println("Cel mai mare divizor comun este: " + m);
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        //cmmdc a doua numere natulra a caror valoare maxima este 30
+        Random random = new Random();
+        int n1 = random.nextInt(30);
+        if(n1 == 0)
+        {
+            n1 += 1;
+        }
+        System.out.println(n1);
+        int n2 = random.nextInt(30);
+        if(n2 == 0)
+        {
+            n2 += 1;
+        }
+        System.out.println(n2);
+        Cmmdc(n1,n2);
+
+    }
+}
